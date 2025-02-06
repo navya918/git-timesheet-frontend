556b071ee62ddfea4b8f53274a80994411c6dfc1
navya918 <139107854+navya918@users.noreply.github.com>
2025-02-06 14:04:45 +0530

    Update EmployeeHomePage.jsx

--- src/components/EmployeeHomePage.jsx
+++ src/components/EmployeeHomePage.jsx
@@ -14,8 +14,8 @@ const EmployeeHomePage = ({ submissions, setSubmissions, employeeId = 'MTL1021' 
   useEffect(() => {
     const fetchSubmissions = async () => {
       try {
-        const response = await axios.get(`https://web-app-bc.azurewebsites.net/api/timesheets/list/${employeeId}`);
-        #const response = await axios.get(`http://localhost:8080/api/timesheets/list/${employeeId}`);
+        #const response = await axios.get(`https://web-app-bc.azurewebsites.net/api/timesheets/list/${employeeId}`);
+        const response = await axios.get(`http://localhost:8080/api/timesheets/list/${employeeId}`);
         const data = response.data.reverse();
         setSubmissions(data);
         setFilteredSubmissions(data);
@@ -37,8 +37,8 @@ const EmployeeHomePage = ({ submissions, setSubmissions, employeeId = 'MTL1021' 
 
   const handleDeleteTimesheet = async (id) => {
     try {
-      #await axios.delete(`http://localhost:8080/api/timesheets/delete/${id}`);
-      await axios.delete(`https://web-app-bc.azurewebsites.net/api/timesheets/delete/${id}`);
+      await axios.delete(`http://localhost:8080/api/timesheets/delete/${id}`);
+      #await axios.delete(`https://web-app-bc.azurewebsites.net/api/timesheets/delete/${id}`);
       const updatedSubmissions = filteredSubmissions.filter(sub => sub.id !== id);
       setFilteredSubmissions(updatedSubmissions);
       setSubmissions(updatedSubmissions);
